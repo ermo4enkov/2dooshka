c3db6a1c303b345858c0e159418536ac40e0d753
ermo4enkov <ermo4enkov@gmail.com>
2017-11-28 00:10:16 +0200

    Added tsx

---
+++ build/develop/app.js
@@ -0,0 +1,9 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackMissingModule() { throw new Error("Cannot find module \"/Users/ermo4enkov/Projects/2dooshka/src/index.tsx\""); }());
+
+
+/***/ })
+],[0]);
