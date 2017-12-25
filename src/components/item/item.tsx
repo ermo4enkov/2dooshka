5ddf77bc6c7b9cce408f189e180c7cee38ba46da
ermo4enkov <ermo4enkov@gmail.com>
2017-12-25 15:37:38 +0300

    Started svg

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import styled from 'styled-components';
+import Button from '../button/button'
 
 interface ItemProps  {
     name?: string;
@@ -9,6 +10,20 @@ interface ItemState  {
     status?: string;
 }
 
+function Plus(props: ItemProps): JSX.Element{
+    return(
+        <img src="" alt=""/>
+    )
+}
+const StyledItem = styled.div`
+    display: inline-block;
+    width: 100%;
+    border-radius: 3px;
+    padding: 16px;
+    font-size: 16px;
+    background-color: #d0d8dc;
+`
+''
 export class Item extends React.Component<ItemProps, ItemState> {
     constructor(ItemProps, ItemState) {
         super(ItemProps)
@@ -17,9 +32,11 @@ export class Item extends React.Component<ItemProps, ItemState> {
     render() {
 
         return(
-            <div>
+            <StyledItem>
+                +
                 {this.props.name}
-            </div>
+                <Button text="Добавить" example/>
+            </StyledItem>
         )
     }
 }
