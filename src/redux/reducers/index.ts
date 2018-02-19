fde16879b33a0568462b508e6f7e0a40bb7b49af
ermo4enkov <ermojob@gmail.com>
2018-02-19 13:46:02 +0300

    Preparation for redux

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,18 @@
+// import {
+//     CODE_RECIEVED_SUCCESS, CODE_RECIEVED_FAIL, FETCH_REQUEST, SET_USER,
+//     CODE_VERIFY_SUCCESS, CODE_VERIFY_FAIL, USER_SIGN_UP_SUCCESS,
+//     USER_SIGN_UP_FAIL, USER_SIGN_IN_SUCCESS, USER_SIGN_IN_FAIL
+// } from "../utils/constants/user";
+
+
+const initialState = {
+    error: "",
+    login: false,
+    user_type: "guest",
+    fetching: false,
+    data: "",
+    data_user: "",
+    succesVerifyCode: false,
+};
+
+
