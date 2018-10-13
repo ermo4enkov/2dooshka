af0f23cec7dae4e6cee89e90f031a14a70ead215
Roman Ermochenkov <ermo4enkov@gmail.com>
2018-10-13 13:20:05 +0300

    fix: prettier

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-and save to reload.
-          </p>
-          <a>
-            Learn
-          </a>
+            <p>Edit{' '}<code>src/App.js</code>{' '}and save to reload.</p>
         </header>
       </div>
     );
